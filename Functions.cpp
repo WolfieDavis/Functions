5fd29bb247c131ee334f1aaea1adf031db02dbe7
Wolfie Davis <wolfiedavis@gmail.com>
2016-05-05 17:00:18 -0400

    Create Functions.cpp

---
+++ Functions.cpp
@@ -0,0 +1,28 @@
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int subtract(int a, int b)
+{
+	return a - b;
+}
+
+int multiply(int a, int b)
+{
+	return a * b;
+}
+
+float divide(float a, float b)
+{
+	cout << "float version" << endl; 
+
+	return a / b;
+}
+
+int divide(int a, int b)
+{
+	cout << "int version" << endl;
+
+	return a / b;
+}
